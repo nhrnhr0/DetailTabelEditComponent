579951f55cc30ad963a50633e2ee90e9861f4a50
nhrnhr0 <nhrnhr0@gmail.com>
2021-09-26 16:09:03 +0300

    almost working

--- src/TableRow.svelte
+++ src/TableRow.svelte
@@ -7,6 +7,27 @@
     import {sizesApiData, colorsApiData, productStore} from './stores/stores'
 
     export let entry_id;
+    export function to_json() {
+        let ret = {};
+        ret.id = $dataStore.id;
+        ret.provider = $dataStore.provider;
+        ret.colors = [];
+        for (let i= 0; i < $colorsValue.length; i++) {
+            ret.colors.push($colorsValue[i]['value']);
+        }
+        ret.sizes = [];
+        for (let i = 0; i < $sizesValue.length; i++) {
+            ret.sizes.push($sizesValue[i]['value']);
+        }
+        ret.cost_price = $costPriceValue
+        ret.client_price = $clientPriceValue
+        ret.recomended_price = $recomendedPriceValue
+        ret.parent = $dataStore.parent
+        return ret;
+    }
+    let costPriceValue = new writable([]);
+    let clientPriceValue = new writable([]);
+    let recomendedPriceValue = new writable([]);
     let sizesValue = new writable([]);
     let colorsValue = new writable([]);
     let sizes_options = [];
@@ -32,19 +53,34 @@
             .then(result => {
                 console.log('got data from url ', tabelDataUrl, ' result: ', result);
                 dataStore.set(result);
-                debugger;
                 let sizes = convert_api_to_options($sizesApiData, $dataStore.sizes)
                 sizesValue.set(sizes);
 
                 let colors = convert_api_to_options($colorsApiData, $dataStore.colors)
                 colorsValue.set(colors);
+                get_provider_name();
+
+                costPriceValue.set($dataStore.cost_price);
+                clientPriceValue.set($dataStore.client_price);
+                recomendedPriceValue.set($dataStore.recomended_price);
             })
             .catch(error => console.log('error', error));
     }
     get_details_from_server();
+    let providerName;
+    function get_provider_name() {
+        let url = ENDPOINT_API + 'svelte/api/providers/' + $dataStore.provider + '/';
+        fetch(url)
+            .then(response => response.json())
+            .then(result => {
+                providerName = result.label;
+                console.log('providerName: ',providerName);
+            })
+            .catch(error => console.log('error', error));
+    }
 
     // limit options to parent sizes
-    $: {
+    
         /*let temp = []
         for(let i =0; i < $sizesApiData.length; i++) {
             for(let j = 0; j < $productStore.sizes.length;j++) {
@@ -54,24 +90,37 @@
             }
         }
         sizes_options = temp;*/
+    $: {
         let sizes = convert_api_to_options($sizesApiData, $productStore.sizes);
         sizes_options = sizes;
+    }
+    $: {
         let colors = convert_api_to_options($colorsApiData, $productStore.colors);
         colors_options = colors;
     }
+    
 </script>
 <tr>
     <td>
         {entry_id}
     </td>
     <td>
-        {$dataStore.providerName}
+        {providerName}
     </td>
     <td>
         <Select items={sizes_options} bind:value={$sizesValue} isMulti={true} ></Select>
     </td>
     <td>
         <Select items={colors_options} bind:value={$colorsValue} isMulti={true} ></Select>
+    </td>
+    <td>
+        <input type="number" bind:value={$costPriceValue}>
+    </td>
+    <td>
+        <input type="number" bind:value={$clientPriceValue}>
+    </td>
+    <td>
+        <input type="number" bind:value={$recomendedPriceValue}>
     </td>
 </tr>
 
@@ -80,7 +129,7 @@
   border: 1px solid black;
 }
 td {
-    margin: 25px;
-    padding: 25px;
+    margin: 5px;
+    padding: 5px;
 }
 </style>
--- src/prog.svelte
+++ src/prog.svelte
@@ -1,10 +1,34 @@
-
 <script>
-    import {ENDPOINT_API} from './utils/consts'
-    import TableRow from './TableRow.svelte'
-    import {sizesApiData, colorsApiData, productStore} from './stores/stores'
-    export let catalog_image_id = '77';
+    import {
+        DialogOverlay,
+        DialogContent
+    } from 'svelte-accessible-dialog';
+    import {
+        ENDPOINT_API
+    } from './utils/consts';
+    import TableRow from './TableRow.svelte';
+    import {
+        sizesApiData,
+        colorsApiData,
+        productStore
+    } from './stores/stores';
+    import {
+        NotificationDisplay,
+        notifier
+    } from '@beyonk/svelte-notifications';
 
+    export let catalog_image_id = '129';
+    let notification_timeout = 3500;
+    let rows = [];
+    let username, password;
+    let isModalOpen;
+    const openModal = () => {
+        isModalOpen = true;
+    };
+
+    const closeModal = () => {
+        isModalOpen = false;
+    };
 
     function get_product_from_server() {
         let catalogImageUrl = ENDPOINT_API + 'svelte/api/products/' + catalog_image_id + '/';
@@ -15,8 +39,9 @@
                 productStore.set(result);
             })
             .catch(error => console.log('error', error));
-            
+
     }
+
     function get_all_sizes() {
         let url = ENDPOINT_API + 'svelte/api/sizes/'
         fetch(url)
@@ -27,6 +52,7 @@
             })
             .catch(error => console.log('error', error));
     }
+
     function get_all_colors() {
         let url = ENDPOINT_API + 'svelte/api/colors/'
         fetch(url)
@@ -40,38 +66,143 @@
     get_all_colors();
     get_all_sizes();
     get_product_from_server();
+
     function saveToServer() {
-        console.log('saveing')
+        console.log('saveing');
+        console.log('rows: ', rows)
+        for (let i = 0; i < rows.length; i++) {
+            let row = rows[i];
+            let json = row.to_json();
+            console.log('row json: ', json)
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+            let token = window.localStorage.getItem('token');
+            myHeaders.append("Authorization", "Bearer " + token);
+            var raw = JSON.stringify(json);
+            var requestOptions = {
+                method: 'PUT',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+            let url = ENDPOINT_API + "svelte/api/productTabel/" + json.id + "/"
+            fetch(url, requestOptions)
+                .then(response => response.json())
+                .then(result => {
+                    debugger;
+                    console.log("result of " + url + " is: " + result);
+                    if (result.code == 'token_not_valid') {
+                        notifier.danger('token_not_valid', notification_timeout);
+
+                        openModal();
+                    } else {
+                        notifier.success(`POST to ${url}, ${result}`, notification_timeout);
+                    }
+                })
+                .catch(error => {
+                    console.log('error', error)
+                    notifier.danger(`POST to ${url} failed`, notification_timeout);
+                });
+        }
+
+
+
     }
+
+    function handle_login() {
+        var myHeaders = new Headers();
+        let url = ENDPOINT_API + "api/token/";
+        var formdata = new FormData();
+        formdata.append("username", username);
+        formdata.append("password", password);
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: formdata,
+            redirect: 'follow'
+        };
+        fetch(url, requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                debugger;
+                console.log("result of " + url + " is: " + result);
+                if(result.access) {
+                    notifier.success(`success obtaining token ${result.access}`, notification_timeout);
+                    window.localStorage.setItem('token', result.access);
+                }else {
+                    notifier.danger(`failed to get token: ${result}`, notification_timeout);
+                }
+            })
+            .catch(error => {
+                console.log('error', error)
+                notifier.danger(`POST to ${url} failed`, notification_timeout);
+            });
+    }
+    let n;
 </script>
 
+<DialogOverlay isOpen={isModalOpen} onDismiss={closeModal}>
+    <DialogContent aria-label="Announcement">
+        <button on:click={closeModal}>Close</button>
+        <form id="login_form" action="{ENDPOINT_API + 'api/token/'}" method="POST">
+            <label for="username">שם משתמש: </label>
+            <input type="text" name="username" bind:value={username}>
+            <label for="password">סיסמא: </label>
+            <input type="password" name="password" bind:value={password}>
+            <button type="button" on:click|preventDefault={handle_login}>Get Token</button>
+        </form>
+    </DialogContent>
+</DialogOverlay>
+
 <main>
-    <img class="sm-img" src={$productStore.image} alt={$productStore.title}/>
+    <img class="sm-img" src={$productStore.image} alt={$productStore.title} />
     <h1>{$productStore.title}</h1>
     {#if $productStore.detailTabel}
         <table>
             <tr>
-                <td>id</td>
-                <td>ספק</td>
-                <td>מידות</td>
-                <td>צבעים</td>
+                <th>id</th>
+                <th>ספק</th>
+                <th>מידות</th>
+                <th>צבעים</th>
+                <th>מחיר עלות</th>
+                <th>מחיר לחנות</th>
+                <th>מחיר מומלץ</th>
             </tr>
-            {#each $productStore.detailTabel as entryId}
-                <TableRow entry_id={entryId}/>
+            {#each $productStore.detailTabel as entryId, i}
+                <TableRow entry_id={entryId} bind:this={rows[i]}/>
             {/each}
         </table>
-        <button type="button" on:click={saveToServer}>שמור</button>
+        <button type="button" on:click={saveToServer}>שמור שינויים בתת טבלה</button>
     {/if}
 </main>
-
-<style>
+<NotificationDisplay bind:this={n} />
+<style lang="scss">
     .sm-img{
         width:150px;
     }
     main {
         direction: rtl;
     }
+    th {
+        font-size: x-large;
+        text-decoration: underline;
+        font-weight: bold;
+        border-bottom: 1px solid black;
+    }
+    table, tr {
+        text-align: center;
+        border-bottom: 1px solid black;
+        max-width: 100vw;
+    }
     table tr {
         border-bottom: 1px solid black;
+    }
+
+    #login_form {
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        direction:rtl;
     }
 </style>
